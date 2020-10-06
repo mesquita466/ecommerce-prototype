c060807f39251d78bec6a1fc0ee33867654280d6
Lucas Mesquita <lucasmesquita58@hotmail.com>
2020-10-06 17:09:58 -0300

    title bug fixed

--- logged.php
+++ logged.php
@@ -5,7 +5,7 @@
 <!doctype html>
 <html lang="pt-br">
   <head>
-    <title><?php echo($_SESSION["username"]); ?></title>
+    <title><?php echo($_SESSION["name"]); ?></title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
